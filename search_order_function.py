7d779165f5c1ea2328aa6d5ecd150545d915216d
Sri Veerisetti <98369110+sveerisetti10@users.noreply.github.com>
2023-04-20 16:26:10 -0400

    Update search_order_function.py

--- search_order_function.py
+++ search_order_function.py
@@ -15,6 +15,7 @@ connection = pymysql.connect(
 )
 
 # Define SQL query
+# Here we allow the user to enter in a value for what table information they would like to display
 query1 = """
 SELECT repeat_type as Transposon_Order, count(*) as Total
 FROM `{}` 
@@ -24,17 +25,19 @@ ORDER BY Total DESC
 
 # Process form data
 form = cgi.FieldStorage()
+#Checking to see that both the selector and the table of choice are indeed valid inputs
 if "selector" in form and "table_choice" in form:
     selector = form.getvalue("selector", "")
     table_choice = form.getvalue("table_choice")
 
-    # If selector is "piechart", execute query and return results as JSON
+    # If the selector is "piechart", then we need to execute the query about and return a JSON file 
     if selector == "piechart":
         try:
             with connection.cursor(pymysql.cursors.DictCursor) as cursor:
                 cursor.execute(query1.format(table_choice))
                 results = cursor.fetchall()
                 print("Content-type: application/json\n")
+                #Create our JSON file that contains the data needed to produce the pie chart
                 print(json.dumps(results))
         except (pymysql.Error, TypeError) as e:
             print("Content-type: text/plain\n")
