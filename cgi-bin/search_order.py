04b736c0610d6e1cc744e1233c3087d5894c4d8d
PoojaS <115026561+poojas4998@users.noreply.github.com>
2023-04-24 17:52:31 -0400

    latest cgi files

---
+++ cgi-bin/search_order.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import json
+import pymysql
+import cgi
+
+# Connect to database
+connection = pymysql.connect(
+    host="bioed.bu.edu",
+    user="sv",
+    password="sv",
+    db="Team_H",
+    port=4253,
+    autocommit=True,
+)
+
+# Define SQL query
+query1 = """
+SELECT repeat_type as Transposon_Order, count(*) as Total
+FROM `{}` 
+GROUP BY repeat_type
+ORDER BY Total DESC
+"""
+
+# Process form data
+form = cgi.FieldStorage()
+if "selector" in form and "table_choice" in form:
+    selector = form.getvalue("selector", "")
+    table_choice = form.getvalue("table_choice")
+
+    # If selector is "piechart", execute query and return results as JSON
+    if selector == "piechart":
+        try:
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                cursor.execute(query1.format(table_choice))
+                results = cursor.fetchall()
+                print("Content-type: application/json\n")
+                print(json.dumps(results))
+        except (pymysql.Error, TypeError) as e:
+            print("Content-type: text/plain\n")
+            print(f"An error occurred: {e}")
